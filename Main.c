5873fd7607e68bf0ee82546ad08f6bf6ad9ec046
NoviaAngreini <131504682+NoviaAngreini@users.noreply.github.com>
2023-05-03 17:46:23 +0700

    Update Main.c

--- Main.c
+++ Main.c
@@ -57,6 +57,40 @@ int main(int banyakArgumen, char *argumen[])
 
     }else if(banyakArgumen == 3){//untuk melakukan login
 
+     char usernameinput[10],passwordinput[10];
+    strcpy(usernameinput, argumen[1]);
+    strcpy(passwordinput, argumen[2]);
+
+    //membaca isi dalam file login
+    FILE *fpr;
+    if((fpr = fopen("database/login.bin", "rb")) == NULL){
+        printf("gagal membuka file");
+        return EXIT_FAILURE;
+    }
+    char akun[20];
+    fread(akun,sizeof(char),sizeof(akun)/sizeof(char), fpr);
+
+    fclose(fpr);
+
+    char *string[3];
+    char username[20], password[20];
+    int ctrl = 0;
+
+    string[0] = strtok(akun, "@");
+    while(string[ctrl++] != NULL){
+        string[ctrl] = strok(NULL,"@");
+
+    }
+    strcpy(username, string[0]);
+    strcpy(password, string[1]);
+
+    if( (strcamp(usernameinput, username) == 0) &&(strcmp(passwordinput,password) == 0 )){
+        printf("selamat anda berhasil login !");
+    }
+    else{
+        printf("\nanda gagal login!\n");
+        printf("username atau password anda salah!\n");
+
     }
 
 
