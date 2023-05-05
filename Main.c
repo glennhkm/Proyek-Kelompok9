1dd49016840c8471e439df9355e65a61e04afc22
glennhkm <glenn@mhs.usk.ac.id>
2023-05-05 17:12:15 +0700

    Program game

--- Main.c
+++ Main.c
@@ -78,22 +78,117 @@ int main(int banyakArgumen, char *argumen[])
 
     string[0] = strtok(akun, "@");
     while(string[ctrl++] != NULL){
-        string[ctrl] = strok(NULL,"@");
+        string[ctrl] = strtok(NULL,"@");
 
     }
     strcpy(username, string[0]);
     strcpy(password, string[1]);
 
-    if( (strcamp(usernameinput, username) == 0) &&(strcmp(passwordinput,password) == 0 )){
+    if( (strcmp(usernameinput, username) == 0) &&(strcmp(passwordinput,password) == 0 )){
         printf("selamat anda berhasil login !");
     }
     else{
-        printf("\nanda gagal login!\n");
-        printf("username atau password anda salah!\n");
-
+        printf("\nAnda gagal login!\n");
+        printf("Username atau password anda salah!\n");
+    }
     }
 
 
+    //game
+    char mulai[20], jawab1[20], jawab2[20], jawab3[20], jawab4[20], jawab5[20];
+    int benar=0, salah=0, uang=0;
+    printf("\n\nSELAMAT DATANG DI PERMAINAN \"ORANG KAYA!\"");
+    printf("\nDalam permainan ini anda akan diberikan 5 pertanyaan dengan hadiah uang tunai \nyang akan terus bertambah setiap pertanyaannya");
+    
+    printf("\n\nTekan 1 untuk memulai permainan : ");
+    gets(mulai);
+     while(mulai[0]>'1' || mulai[0]<'0' || strlen(mulai)>1){
+        printf("Tolong masukan pilihan yang tersedia (1), dan jangan menggunakan spasi!");        
+        printf("\nTekan 1 untuk memulai permainan : ");
+        gets(mulai);
+        fflush(stdin);
+    }
+
+    if(mulai[0]=='1'){
+        system("cls");
+        printf("Bersiaplah menjadi ORANG KAYA!!!\n\n");
+        for(int i=160000; i>2; i--){
+                if(i/40000==0){
+                    continue;
+                }
+                printf("\rGame akan di mulai dalam : %d", i/40000);
+            }
+        system("cls");
+        //Soal no 1
+        printf("Hadiah untuk soal ini = Rp500.000");
+        printf("\nUang yang telah anda kumpulkan = Rp%d", uang);
+        printf("\n\n1. MinGW merupakan compiler yang biasa digunakan untuk bahasa pemrograman...?");
+        printf("\n\tA. C\n");
+        printf("\tB. Javascript\n");
+        printf("\tC. Phyton\n");
+        printf("Jawaban anda (A/B/C) : ");
+        gets(jawab1);
+        fflush(stdin); 
+        while(jawab1[0]<'A' || jawab1[0]>'C' || strlen(jawab1)>1){
+            printf("Tolong masukan pilihan yang tersedia (A/B/C), gunakan huruf kapital, dan jangan menggunakan spasi!");
+            printf("\nJawaban anda (A/B/C) : ");
+            gets(jawab1);
+            fflush(stdin);
+        }
+        if(jawab1[0]== 'A'){
+            printf("Selamat jawaban anda benar, bersiaplah menjawab soal selanjutnya\n\n");
+            for(int i=160000; i>2; i--){
+                if(i/40000==0){
+                    continue;
+                }
+                printf("\r%d", i/40000);
+            }
+            uang+=500000;
+            system("cls");
+        }
+        else{
+            printf("\nJawaban anda salah, uang yang anda bawa pulang senilai Rp%d");
+            printf("\nTerima kasih telah memainkan permainan ini, sampai jumpa dan jadilah ORANG KAYA!!!");
+            uang+=0;
+            exit(1);
+        }
+
+        //Soal 2
+        printf("Hadiah untuk soal ini = Rp1.000.000");
+        printf("\nUang yang telah anda kumpulkan = Rp%d", uang);
+        printf("\n\n2. Tahun berapa bahasa C ditemukan?");
+        printf("\n\tA. 1970\n");
+        printf("\tB. 1972\n");
+        printf("\tC. 1979\n");
+        printf("Jawaban anda (A/B/C) : ");
+        gets(jawab2);
+        fflush(stdin); 
+        while(jawab2[0]<'A' || jawab2[0]>'C' || strlen(jawab2)>1){
+            printf("Tolong masukan pilihan yang tersedia (A/B/C), gunakan huruf kapital, dan jangan menggunakan spasi!");
+            printf("\nJawaban anda (A/B/C) : ");
+            gets(jawab2);
+            fflush(stdin);
+        }
+        if(jawab2[0]== 'B'){
+            printf("Selamat jawaban anda benar, bersiaplah menjawab soal selanjutnya\n\n");
+            for(int i=160000; i>2; i--){
+                if(i/40000==0){
+                    continue;
+                }
+                printf("\r%d", i/40000);
+            }
+            uang+=1000000;
+            system("cls");
+        }
+        else{
+            printf("\nJawaban anda salah, uang yang anda bawa pulang senilai Rp%d");
+            printf("\nTerima kasih telah memainkan permainan ini, sampai jumpa dan jadilah ORANG KAYA!!!");
+            uang+=0;
+            exit(1);
+        }
+    }
+
+    
     return 0;
 
 }
